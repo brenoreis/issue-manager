7804138c16af87a8a9ac65ee923963a872935136
Breno.Pinto <breno.pinto@healthdirect.org.au>
2017-05-13 18:29:04 +1000

    deleting files

--- PersonalFinance/src/main/java/personalFinance/service/exception/CategoryAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package personalFinance.service.exception;
-
-public class CategoryAlreadyExistsException extends RuntimeException{
-
-    public CategoryAlreadyExistsException(final String message) {
-        super(message);
-    }
-
-}
